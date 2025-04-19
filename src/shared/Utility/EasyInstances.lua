7c69ee5a98ac5b9c333b5754c18e7b6023d249de
Leifeen <Leifeen3@gmail.com>
2025-04-18 21:08:06 -0300

    Solo Rise 1.1
    Added Wall Climb and more stuff into State Machine.

--- src/shared/Utility/EasyInstances.lua
+++ src/shared/Utility/EasyInstances.lua
@@ -6,6 +6,25 @@ EasyInstances.DestroyWDebris = function(Int, TimerTo)
     EasyInstances.Debris:AddItem(Int, TimerTo)
 end
 
+EasyInstances.CreateAlignOrient = function(Char)
+        
+    local AO = Instance.new("AlignOrientation")
+    AO.Parent = Char.HumanoidRootPart
+    AO.Name = "ForceOrient"
+
+    local NewAttach = Instance.new("Attachment")
+    NewAttach.Parent = Char.HumanoidRootPart
+    
+    local NewAttach2 = Instance.new("Attachment")
+    NewAttach2.Parent = Char.HumanoidRootPart
+    
+
+    AO.Attachment0 = NewAttach
+    AO.Attachment1 = NewAttach2
+
+    return AO, NewAttach, NewAttach2
+
+end
 
 EasyInstances.CreateAlignPosIntoChar = function(Char)
       
